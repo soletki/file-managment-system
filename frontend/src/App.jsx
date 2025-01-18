a0c2895b8d6cb205aa3ae36cc6ec233560347103
ToonLoL <st.v.ivanov07@gmail.com>
2025-01-18 16:43:40 +0200

    ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import FileTable from './components/FileTable/FileTable';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [file, setFile] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const uploadFileRef = useRef(null);
+    async function triggerUploadFile(){
+        await uploadFileRef.current.click();
+    };
+
+    const updateFileRef = useRef(null);
+    async function triggerUpdateFile(){
+        await updateFileRef.current.click();
+    };
+
+    async function uploadFile(event) {
+        const uploadedFile = event.target.files[0];
+        if (uploadedFile) {
+            setFile(uploadedFile.name);
+            console.log(`Uploaded file: ${uploadedFile.name}`);
+        }
+    }
+    async function getFile() {
+        prompt("File id:")
+    }
+    async function updateFile(event) {
+        const uploadedFile = event.target.files[0];
+        if (uploadedFile) {
+            setFile(uploadedFile.name);
+            console.log(`Uploaded file: ${uploadedFile.name}`);
+        }
+    }
+    async function deleteFile() {
+        prompt("File id:")
+    }
+
+
+	return (
+		<>
+			<div id="top">
+				<h1 id="main-title">File Managment System</h1>
+				<img id="pfp" src="../public/profile-user-account.svg" />
+			</div>
+
+			<section>
+				<div onClick={triggerUploadFile}>
+					<img src="../public/upload-sign.svg"/>
+                    <input type='file' ref={uploadFileRef} style={{ display: 'none' }} onChange={(e) => uploadFile(e)}></input>
+					<h2>POST</h2>
+				</div>
+				<div onClick={getFile}>
+					<img src="../public/download-button.svg"/>
+					<h2>GET</h2>
+				</div>
+				<div onClick={triggerUpdateFile}>
+					<img src="../public/update-icon.svg"/>
+                    <input type='file' ref={updateFileRef} style={{ display: 'none' }} onChange={(e) => updateFile(e)}></input>
+					<h2>UPDATE</h2>
+				</div>
+				<div onClick={deleteFile}>
+					<img src="../public/delete-button.svg"/>
+					<h2>DELETE</h2>
+				</div>
+			</section>
+
+            <FileTable></FileTable>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/FileTable/FileTable.jsx
@@ -0,0 +1,56 @@
+import "./FileTable.css"
+
+function FileTable() {
+	return (
+        <>
+            <table>
+				<thead>
+					<tr>
+						<th>File ID</th>
+						<th>File Name</th>
+						<th>File Size</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>example.txt</td>
+						<td>14 KB</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>data.csv</td>
+						<td>32 KB</td>
+					</tr>
+					<tr>
+						<td>3</td>
+						<td>report.pdf</td>
+						<td>1.2 MB</td>
+					</tr>
+                    <tr>
+						<td>3</td>
+						<td>report.pdf</td>
+						<td>1.2 MB</td>
+					</tr>
+                    <tr>
+						<td>3</td>
+						<td>report.pdf</td>
+						<td>1.2 MB</td>
+					</tr>
+                    <tr>
+						<td>3</td>
+						<td>report.pdf</td>
+						<td>1.2 MB</td>
+					</tr>
+                    <tr>
+						<td>3</td>
+						<td>report.pdf</td>
+						<td>1.2 MB</td>
+					</tr>
+				</tbody>
+			</table>
+        </>
+    )
+}
+
+export default FileTable
